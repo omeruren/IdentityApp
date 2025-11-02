74838766c923a28c534d30ac82446b2aac328342
Ömer Üren <omer.uren19@gmail.com>
2025-11-02 21:35:57 +0300

    Sign Up page
    Created signup view model

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
     }
 }
---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+        [Display(Name = "User Name :")]
+        public string UserName { get; set; }
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+        [Display(Name = "Phone :")]
+        public string Phone { get; set; }
+        [Display(Name = "Password :")]
+        public string Password { get; set; }
+        [Display(Name = "Repeat Password :")]
+        public string PasswordConfirm { get; set; }
+    }
+}

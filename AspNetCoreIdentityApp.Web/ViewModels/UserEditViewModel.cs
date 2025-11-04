ebc87925506041fcb7825025c2871f7112d338fe
Ömer Üren <omer.uren19@gmail.com>
2025-11-04 10:17:20 +0300

    Edit User View Model

---
+++ AspNetCoreIdentityApp.Web/ViewModels/UserEditViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class UserEditViewModel
+    {
+        [Required(ErrorMessage = "User name is required")]
+        [Display(Name = "User Name :")]
+        public string UserName { get; set; } = null!;
+
+        [EmailAddress(ErrorMessage = "Invalid Format")]
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Email :")]
+        public string Email { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Display(Name = "Phone :")]
+        public string Phone { get; set; } = null!;
+
+        [Display(Name = "Birth Date :")]
+        public string? BirthDate { get; set; }
+
+        [Display(Name = "City :")]
+        public string? City { get; set; }
+
+        [Display(Name = "Profile Picture :")]
+        public IFormFile? Picture { get; set; }
+
+        [Display(Name = "Gender :")]
+        public byte? Gender { get; set; }
+
+    }
+}

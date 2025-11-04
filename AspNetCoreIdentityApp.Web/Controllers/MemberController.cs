aaaa20e1c72c7aab0d49892cee91bb4a32fdd714
Ömer Üren <omer.uren19@gmail.com>
2025-11-04 13:17:21 +0300

    Img Url With Tag helper

--- AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -26,7 +26,7 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         {
             var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
 
-            var userViewModel = new UserViewModel { Email = currentUser!.Email, Phone = currentUser.PhoneNumber, UserName = currentUser.UserName };
+            var userViewModel = new UserViewModel { Email = currentUser!.Email, Phone = currentUser.PhoneNumber, UserName = currentUser.UserName,PictureUrl= currentUser.Picture };
 
             return View(userViewModel);
         }
